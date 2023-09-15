10ea3094efb3a4b162167acd1820c10a0ddaf3cd
CodeXzzz <codexzzz@outlook.com>
2023-09-15 16:29:02 +0800

    [test][mock]:create server.go's mock and update server.go's interfaces

--- server.go
+++ server.go
@@ -6,15 +6,6 @@ import (
 	"time"
 )
 
-const (
-	OpContinuation OpCode = 0x0
-	OpText         OpCode = 0x1
-	OpBinary       OpCode = 0x2
-	OpClose        OpCode = 0x8
-	OpPing         OpCode = 0x9
-	OpPong         OpCode = 0xa
-)
-
 type Server interface {
 	SetAcceptor(Acceptor)
 	SetMessageListener(MessageListener)
@@ -27,12 +18,14 @@ type Server interface {
 	Shutdown(ctx context.Context) error
 }
 
-// Acceptor Start()监听到连接后，调用此方法让上层业务处理握手
+// Acceptor Start()监听到连接后，调用此方法让业务层处理握手
 type Acceptor interface {
+	// Accept 返回error则断开连接
 	Accept(Conn, time.Duration) (string, error)
 }
 
-type SetStateListener interface {
+// StateListener 报告断开连接的事件
+type StateListener interface {
 	Disconnect(string) error
 }
 
@@ -40,10 +33,24 @@ type MessageListener interface {
 	Receive(Agent, []byte)
 }
 
+// Agent 消息发送方
 type Agent interface {
 	ID() string
 	Push([]byte) error
 }
+
+type OpCode byte
+
+const (
+	OpContinuation OpCode = 0x0
+	OpText         OpCode = 0x1
+	OpBinary       OpCode = 0x2
+	OpClose        OpCode = 0x8
+	OpPing         OpCode = 0x9
+	OpPong         OpCode = 0xa
+)
+
+// Frame 解决tcp流式传输导致的封包与拆包
 type Frame interface {
 	SetOpCode(OpCode)
 	GetOpCode() OpCode
@@ -51,10 +58,31 @@ type Frame interface {
 	GetPayload() []byte
 }
 
-// Conn Connection
+// Conn 封装tcp读写操作
 type Conn interface {
 	net.Conn
 	ReadFrame() (Frame, error)
 	WriteFrame(OpCode, []byte) error
 	Flush() error
+}
+
+type Client interface {
+	ID() string
+	Name() string
+	Connect(string) error
+	// SetDialer 由Connect调用，完成连接的握手和建立
+	SetDialer(Dialer)
+	Send([]byte) error
+	// Read 读取一帧数据，底层复用了X.Conn，因此返回Frame
+	Read() (Frame, error)
+	Close()
+}
+type Dialer interface {
+	DialAndHandshake(DialerContext) (net.Conn, error)
+}
+type DialerContext struct {
+	Id      string
+	Name    string
+	Address string
+	Timeout time.Duration
 }
